a0572608781df1147be4b5b093db16cf9bc97457
Srikanth-36 <93000461+Srikanth-36@users.noreply.github.com>
2021-11-04 11:19:44 +0530

    Update logomaker.py

--- MashaRoBot/modules/logomaker.py
+++ MashaRoBot/modules/logomaker.py
@@ -100,11 +100,11 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
     fname2 = "LogoByInnexia.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="💕 Made By @LOVELYR_OBOT Support @LOVELYAPPEAL")
+    await tbot.send_file(event.chat_id, fname2, caption="💕 Made By @Sriki_Ro_bot Support @MASTI_WORLD_CHATTING")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Report @LOVELYAPPEAL, {e}')
+   await event.reply(f'Error Report @sriki_Supporters, {e}')
 
 
 
