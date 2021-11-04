d4f4993996e357bed97fdb1441ff5b7428660d89
Srikanth-36 <93000461+Srikanth-36@users.noreply.github.com>
2021-11-04 11:22:16 +0530

    Update alive.py

--- MashaRoBot/modules/alive.py
+++ MashaRoBot/modules/alive.py
@@ -7,10 +7,10 @@ PHOTO = "https://telegra.ph/file/63ec234e1be1f16345e69.jpg"
 @register(pattern=("/alive"))
 async def awake(event):
   LOVELY = event.sender.first_name
-  LOVELY = "**♡ I,m Lovely💕** \n\n"
+  LOVELY = "**♡ I,m Sriki💕** \n\n"
   LOVELY += "**♡ I'm Working with awesome speed**\n\n"
   LOVELY += "**♡ Lovely : 2.0 LATEST**\n\n"
-  LOVELY += "**♡ My Creator :** [𝗧𝗨𝗦𝗛𝗔𝗥😍](t.me/Tushar204)\n\n"
+  LOVELY += "**♡ My Creator :** [𝗦𝗿𝗶𝗸𝗮𝗻𝘁𝗵😍](t.me/Srikanth_36)\n\n"
   LOVELY += "**♡ Telethon Version : 1.23.0**\n\n"
-  BUTTON = [[Button.url("𝐒𝐔𝐏𝐏𝐎𝐑𝐓🙂", "https://t.me/LOVELYAPPEAL"), Button.url("𝐔𝐏𝐃𝐀𝐓𝐄", "https://t.me/LOVELY_ROBOTS")]]
+  BUTTON = [[Button.url("𝐒𝐔𝐏𝐏𝐎𝐑𝐓🙂", "https://t.me/Masti_world_chatting"), Button.url("𝐔𝐏𝐃𝐀𝐓𝐄", "https://t.me/Sriki_BOTS")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=LOVELY,  buttons=BUTTON)
